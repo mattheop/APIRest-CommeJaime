141df6d485e153048d48020a6ca1f094e9f44935
Matthéo PERELLE <mattheo.perelle@gmail.com>
2023-03-10 10:42:25 +0100

    add schema.sql

---
+++ sql/schema.sql
@@ -0,0 +1,37 @@
+create database if not exists commejaime;
+use commejaime;
+
+create table if not exists users
+(
+    id_user  int                                       not null auto_increment,
+    username varchar(50)                               not null,
+    password varchar(100)                              not null,
+    role     enum ('ROLE_PUBLISHER', 'ROLE_MODERATOR') not null default 'ROLE_PUBLISHER',
+
+    primary key (id_user)
+);
+
+create table if not exists posts
+(
+    id_post    int          not null auto_increment,
+    title      varchar(100) not null,
+    content    text         not null,
+    created_at datetime     not null,
+    id_user    int          not null,
+
+    primary key (id_post),
+    foreign key (id_user) references users (id_user),
+
+    constraint title_start_uppercase_or_digit check (title regexp '^[A-Z0-9]')
+);
+
+create table if not exists liked
+(
+    id_post int  not null,
+    id_user int  not null,
+    is_up   bool not null default true,
+
+    primary key (id_post, id_user),
+    foreign key (id_post) references posts (id_post),
+    foreign key (id_user) references users (id_user)
+);
