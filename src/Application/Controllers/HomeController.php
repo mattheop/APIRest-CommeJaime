24f5b923450f341956bdd6169bf5aaa7a6021418
Anna NIETO <anna.nieto@etu.iut-tlse3.fr>
2023-03-16 08:33:34 +0100

    fetch all

--- src/Application/Controllers/HomeController.php
+++ src/Application/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Application\Controllers;
 
+use App\Application\ORM\Database;
+use PDO;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -10,6 +12,8 @@ class HomeController
 
     public function index(ServerRequestInterface $request, ResponseInterface $response)
     {
+        dump(Database::getInstance()->getPDO()->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC));
+        dd(Database::getInstance()->getPDO()->errorInfo());
         return $response->withStatus(404);
     }
 
