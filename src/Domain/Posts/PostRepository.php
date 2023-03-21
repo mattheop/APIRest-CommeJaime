e4db707c1161d8ea6288774892a829fd8a9e983c
Matthéo PERELLE <mattheo.perelle@gmail.com>
2023-03-21 20:53:06 +0100

    add pagination

--- src/Domain/Posts/PostRepository.php
+++ src/Domain/Posts/PostRepository.php
@@ -2,14 +2,13 @@
 
 namespace App\Domain\Posts;
 
-use App\Application\Application;
 use App\Application\ORM\Database;
 use App\Application\ORM\RowMapper;
 use PDO;
 
 class PostRepository
 {
-
+    public const FETCH_ALL_LIMIT = 15;
     private RowMapper $rowMapper;
 
     public function __construct()
@@ -21,9 +20,18 @@ class PostRepository
      * Permet de récupérer tous les posts depuis la base de données.
      * @return PostsModel[]
      */
-    public function fetchAll(): array
+    public function fetchAll(?int $limit = null, ?int $page = null): array
     {
-        $statement = Database::getInstance()->getPDO()->prepare("SELECT * FROM posts");
+        $limit ??= self::FETCH_ALL_LIMIT;
+        $page ??= 1;
+
+        $offset = ($page - 1) * $limit;
+
+        $statement = Database::getInstance()->getPDO()->prepare("SELECT * FROM posts LIMIT ? OFFSET ?");
+
+        $statement->bindParam(1, $limit, PDO::PARAM_INT);
+        $statement->bindParam(2, $offset, PDO::PARAM_INT);
+
         $statement->execute();
 
         return array_map([$this->rowMapper, 'map'], $statement->fetchAll(PDO::FETCH_ASSOC));
